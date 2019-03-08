ca59f5b6438426145669a6168b392fbf08857e88
csw001 <eudemonia_c@foxmail.com>
2019-03-08 17:18:11 +0800

    'fisrt'

---
+++ config/site.config.js
@@ -0,0 +1,12 @@
+const site = {
+    eudemonia: {
+        name: 'react-cli',
+        title: 'react脚手架',
+        short_title: 'eudemonia',
+        host: 'eudemonia.ecaicn.com',
+        port: 8888,
+        api: '127.0.0.1',
+    }
+}
+
+module.exports = site;
---
+++ src/container/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.module.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        123
+      </div>
+    );
+  }
+}
+
+export default App;
