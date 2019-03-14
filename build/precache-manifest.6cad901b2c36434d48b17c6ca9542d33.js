f29da77f2737ae0465b6643e53842e3b12029e88
csw001 <eudemonia_c@foxmail.com>
2019-03-14 19:14:34 +0800

    '公共路径'

---
+++ build/precache-manifest.6cad901b2c36434d48b17c6ca9542d33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a614ad9d109cf9ed48a5",
+    "url": ".static/js/runtime~main.88e60ccc.js"
+  },
+  {
+    "revision": "524aaa1de120376a8bc0",
+    "url": ".static/js/main.18e476c0.chunk.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": ".static/js/2.aa7a0768.chunk.js"
+  },
+  {
+    "revision": "524aaa1de120376a8bc0",
+    "url": ".static/css/main.98601cc0.chunk.css"
+  },
+  {
+    "revision": "15ce02d1e9e91d468f2a91efde24b5db",
+    "url": ".index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8a16d582d1fd77ad070ecb2a3c9da6be.js"
+  ".precache-manifest.6cad901b2c36434d48b17c6ca9542d33.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
