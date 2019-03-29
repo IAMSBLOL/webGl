e5dc4aaf1b956c7d95bdcd1e92580e99b60f7872
csw001 <eudemonia_c@foxmail.com>
2019-03-29 18:28:10 +0800

    '理清项目依赖，可能存在兼容问题、'

--- src/container/App.js
+++ src/container/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import './App.module.scss';
 
-import Test from './test'
-
 class App extends Component {
     render () {
         return (
             <div styleName='App' className='APP'>
                 123
                 <div className='fucking'>12312312321</div>
-                <Test />
+
             </div>
         );
     }
