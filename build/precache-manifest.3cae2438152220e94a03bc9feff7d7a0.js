44acc9b2ea68b055f695c85be2db6ece0cd5177f
csw001 <eudemonia_c@foxmail.com>
2019-04-02 14:45:03 +0800

    '修改公共路径+3'

---
+++ build/precache-manifest.3cae2438152220e94a03bc9feff7d7a0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "f99d285a0f4228758de3",
+    "url": "react-cli/build/static/css/5.d945c823.chunk.css"
+  },
+  {
+    "revision": "72a287ca15f5fab76ea4",
+    "url": "react-cli/build/static/css/main.8043c7aa.chunk.css"
+  },
+  {
+    "revision": "44d08e52d460762d8275",
+    "url": "react-cli/build/static/js/runtime~main.b3a12b73.js"
+  },
+  {
+    "revision": "b3dcb429a0b454f83bc1ba45af22f300",
+    "url": "react-cli/build/static/media/xingkong.b3dcb429.jpg"
+  },
+  {
+    "revision": "aedb62dbe18a20031793",
+    "url": "react-cli/build/static/js/2.1478c791.chunk.js"
+  },
+  {
+    "revision": "699b8415e9eafd3f6b14",
+    "url": "react-cli/build/static/js/7.809a3bbd.chunk.js"
+  },
+  {
+    "revision": "b1a79b054f27a3367d10",
+    "url": "react-cli/build/static/js/3.cde649d1.chunk.js"
+  },
+  {
+    "revision": "5cfdfe8372c9a856765c",
+    "url": "react-cli/build/static/js/6.e15dc54f.chunk.js"
+  },
+  {
+    "revision": "3db36ed136838a4002f4",
+    "url": "react-cli/build/static/js/4.b5780786.chunk.js"
+  },
+  {
+    "revision": "72a287ca15f5fab76ea4",
+    "url": "react-cli/build/static/js/main.adc53366.chunk.js"
+  },
+  {
+    "revision": "f99d285a0f4228758de3",
+    "url": "react-cli/build/static/js/5.ced0c649.chunk.js"
+  },
+  {
+    "revision": "699b8415e9eafd3f6b14",
+    "url": "react-cli/build/static/css/7.f2a18eb0.chunk.css"
+  },
+  {
+    "revision": "5cfdfe8372c9a856765c",
+    "url": "react-cli/build/static/css/6.2e220550.chunk.css"
+  },
+  {
+    "revision": "3db36ed136838a4002f4",
+    "url": "react-cli/build/static/css/4.ff76489f.chunk.css"
+  },
+  {
+    "revision": "b1a79b054f27a3367d10",
+    "url": "react-cli/build/static/css/3.d2779743.chunk.css"
+  },
+  {
+    "revision": "aedb62dbe18a20031793",
+    "url": "react-cli/build/static/css/2.808660db.chunk.css"
+  },
+  {
+    "revision": "e6a497ce42b15022840dac405cb7e332",
+    "url": "react-cli/build/public\\canvas.js"
+  },
+  {
+    "revision": "931a4f60c2fa20e806b94c997b93e0c6",
+    "url": "react-cli/build/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.935cc036039d722789c2deed05265456.js"
+  "react-cli/build/precache-manifest.3cae2438152220e94a03bc9feff7d7a0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("react-cli/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
