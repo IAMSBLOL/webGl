b5c6fe309cbc59a55290f25a533957729fe352f1
csw001 <eudemonia_c@foxmail.com>
2019-04-02 14:21:41 +0800

    '秘制相对路劲'

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  ".precache-manifest.34ffdb4e7a1bc3e709e4831e84deaec2.js"
+  ".precache-manifest.a98f42d9db01ac1817d498d64c4757b0.js"
 );
 
 workbox.clientsClaim();
